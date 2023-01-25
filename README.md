234c81b981f2edbfe5fc9d7451297fc486528f3d
Trisnox <66292369+Trisnox@users.noreply.github.com>
2023-01-26 01:59:10 +0700

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Lost-Saga-AFK-Auto-Solver
+Macro untuk menyelesaikan mtk yang disebabkan oleh afk. Sebagai alternatif untuk rtl.
+
+Setelah script dijalankan, script akan screenshot setiap beberapa detik yang ditentukan, jika menemukan box mtk maka script ini akan membaca dan menjawab pertanyaan tersebut.
+
+# Cara Pemakaian
+Pergi ke [releases](https://github.com/Trisnox/Lost-Saga-AFK-Auto-Solver/releases) dan pilih versi, lalu ekstrak file. Pastikan semua package sudah di install, jika belum jalankan cmd di folder tersebut lalu ketik `pip install -U -r requirements.txt` untuk install semua package yang dibutuhkan. Setelah itu install juga [Tesseract](https://github.com/UB-Mannheim/tesseract/wiki) dan tambahkan ke PATH jika belum. Jika semua kebutuhan sudah terpenuhi, gunakan cmd di folder tersebut menggunakan admin lalu ketik `main.py`.
+
+Setelah GUI muncul, atur beberapa pilihan yang sesuai dengan yang digunakan, dan setelah selesai mengatur, klik tombol `Mulai` atau F6 di keyboard.
+
+# To-do
+- Resolusi otomatis
+- Perbaiki test input
+- Dark mode
+
+## Note
+Hanya berkeja untuk Lost Saga Origin atau Lost Saga berbahasa indonesia lainnya. Jika ingin menggunakan versi lain, coba tukar gambar komponen yang ada di dalam folder img, jika masih belum bisa modifikasi script mungkin dibutuhkan.
---
+++ main.py
@@ -0,0 +1,462 @@
+import ctypes
+import cv2
+import itertools
+import json
+import multiprocessing
+import numpy as np
+import os
+import pydirectinput
+import pynput
+import pytesseract
+import time
+import tkinter as tk
+import win32gui
+from PIL import Image, ImageGrab, ImageTk
+from tkinter import filedialog, ttk
+
+def is_admin():
+    try:
+        is_admin = (os.getuid() == 0)
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin
+
+if not is_admin():
+    tk.messagebox.showerror(title='Missing Perms', message='Program tidak di run menggunakan admin. Jika program tidak di run dengan admin, kemungkinan tidak bisa input ke gamenya. Mohon run ulang program dengan admin.')
+    exit()
+
+input_process = None
+main_process = None
+toggle = False
+pup_up = None
+
+with open('config.json', 'r') as f:
+    config: dict = json.load(f)
+
+class ImageLabel(tk.Label):
+    """a label that displays images, and plays them if they are gifs"""
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        self.loc = 0
+        self.frames = []
+
+        try:
+            for i in itertools.count(1):
+                self.frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
+
+        if len(self.frames) == 1:
+            self.config(image=self.frames[0])
+        else:
+            self.next_frame()
+
+    def unload(self):
+        self.config(image="")
+        self.frames = None
+
+    def next_frame(self):
+        if self.frames:
+            self.loc += 1
+            self.loc %= len(self.frames)
+            self.config(image=self.frames[self.loc])
+            self.after(self.delay, self.next_frame)
+
+current = set()
+
+def on_press(key):
+    combination = {pynput.keyboard.Key.ctrl, '\x03', 'c'}
+
+    if key == pynput.keyboard.Key.f6:
+        toggle_script()
+    elif key in combination:
+        current.add(key)
+        if all(k in current for k in combination):
+            listener.stop()
+            raise KeyboardInterrupt
+
+def on_release(key):
+    try:
+        current.remove(key)
+    except KeyError:
+        pass
+
+def main():
+    print('Script telah dimulai')
+    pytesseract.pytesseract.tesseract_cmd = config['tesseract']
+
+    while True:
+        time.sleep(config['interval'])
+
+        res = math_loop()
+        
+        if not res:
+            continue
+        
+        ans = str(sum(tuple(map(int, res))))
+        pydirectinput.typewrite(ans, interval=0.025)
+        time.sleep(.1)
+        pydirectinput.press('enter')
+
+def math_loop():
+    img = get_win_ss()
+    img = pre_crop(img)
+    
+    res = image_search(img, './img/afk1.png')
+    if not res:
+        res = image_search(img, './img/afk2.png')
+    
+    if not res:
+        return False
+
+    img = math_crop(img)
+
+    text = ocr()
+
+    num = ocr_check(text)
+
+    if not num:
+        num = math_loop()
+    
+    return num
+
+def get_win_ss():
+    toplist, winlist = [], []
+    def enum_cb(hwnd, results):
+        winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+    win32gui.EnumWindows(enum_cb, toplist)
+
+    lso = [(hwnd, title) for hwnd, title in winlist if 'lost saga in time' in title.lower()]
+    lso = lso[0]
+    hwnd = lso[0]
+
+    win32gui.SetForegroundWindow(hwnd)
+    bbox = win32gui.GetWindowRect(hwnd)
+    img = ImageGrab.grab(bbox)
+    if img:
+        return img
+    else:
+        return False
+
+def pre_crop(img):
+    if not config['window'] == 'full': # windowed mode
+        w, h = img.size
+        if config['resolution'] == '800x600':
+            img = img.crop(((w - 255) // 2, 45, (w + 255) // 2, 171))
+        elif config['resolution'] == '1024x768':
+            img = img.crop(((w - 255) // 2, 50, (w + 255) // 2, 176))
+        elif config['resolution'] == '1280x720':
+            img = img.crop(((w - 255) // 2, 49, (w + 255) // 2, 151))
+        elif config['resolution'] == '1280x1024':
+            img = img.crop(((w - 255) // 2, 59, (w + 255) // 2, 185))
+        elif config['resolution'] == '1600x900':
+            img = img.crop(((w - 255) // 2, 55, (w + 255) // 2, 182))
+        elif config['resolution'] == '1680x1050':
+            img = img.crop(((w - 255) // 2, 60, (w + 255) // 2, 186))
+        elif config['resolution'] == '1920x1080':
+            img = img.crop(((w - 255) // 2, 61, (w + 255) // 2, 187))
+        else:
+            print('unknown resolution selected')
+    else:
+        w, h = img.size
+        if config['resolution'] == '800x600':
+            img = img.crop(((w - 255) // 2, 19, (w + 255) // 2, 145))
+        elif config['resolution'] == '1024x768':
+            img = img.crop(((w - 255) // 2, 24, (w + 255) // 2, 150))
+        elif config['resolution'] == '1280x720':
+            img = img.crop(((w - 255) // 2, 23, (w + 255) // 2, 149))
+        elif config['resolution'] == '1280x1024':
+            img = img.crop(((w - 255) // 2, 33, (w + 255) // 2, 159))
+        elif config['resolution'] == '1600x900':
+            img = img.crop(((w - 255) // 2, 29, (w + 255) // 2, 156))
+        elif config['resolution'] == '1680x1050':
+            img = img.crop(((w - 255) // 2, 34, (w + 255) // 2, 160))
+        elif config['resolution'] == '1920x1080':
+            img = img.crop(((w - 255) // 2, 35, (w + 255) // 2, 161))
+        else:
+            print('unknown resolution selected')
+    
+    return img
+
+def math_crop(img):
+    img = img.crop((40, 67, 41+40, 15+67))
+    w, h = img.size
+    img = img.resize((w*5, h*5), resample=Image.LANCZOS)
+    img.save('./img/text.png')
+    return img
+
+def image_search(ss, target):
+    img_rgb = np.array(ss)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(target, 0)
+    template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    if max_val < 0.8:
+        return None
+
+    return max_loc
+
+def ocr():
+    img = cv2.imread("img/text.png")
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                                     cv2.CHAIN_APPROX_NONE)
+
+    im2 = img.copy()
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cropped = im2[y:y + h, x:x + w]
+
+        text = pytesseract.image_to_string(cropped, config='-c tessedit_char_whitelist=0123456789+- --psm 6 digits')
+        print(text)
+
+    return text
+
+def ocr_check(result):
+    filtered = result.split('+')
+    try:
+        num1 = filtered[0]
+        int(num1)
+        num2 = filtered[1]
+        int(num2)
+    except (ValueError, IndexError):
+        print('OCR gagal mengidentifikasi seluruh nomor, mencoba ulang')
+        pydirectinput.press('1')
+        time.sleep(.1)
+        pydirectinput.press('enter')
+        return False
+
+    if len(num1) >= 3:
+        num1 = num1[2]
+        return (num1, num2)
+    else:
+        return (num1, num2)
+
+def toggle_script():
+    global main_process
+    global toggle
+    global tesseract_loc
+    global interval_input
+    global fix_input
+    global input_input
+    global window_input
+    global resolution_input
+    global toggle_button
+
+    if not toggle:
+        tesseract_loc.set(tesseract_entry.get())
+        interval_input.set(interval_entry.get())
+        
+        if not tesseract_loc.get():
+            tk.messagebox.showerror(title='Invalid', message='Lokasi Tesseract tidak bisa kosong')
+            return False
+        
+        try:
+            float(interval_input.get())
+        except ValueError:
+            tk.messagebox.showerror(title='Invalid', message='Interval harus berupa int atau float')
+            return False
+        
+        config['tesseract'] = tesseract_loc.get().replace('/', '\\')
+        config['fix'] = fix_input.get()
+        config['input'] = input_input.get()
+        config['interval'] = float(interval_input.get())
+        config['window'] = window_input.get()
+        config['resolution'] = resolution_input.get()
+
+        with open('config.json', 'w') as f:
+            json.dump(config, f, indent=4)
+        
+        toggle = True
+        toggle_button.config(text='Stop')
+        main_process = multiprocessing.Process(target=main)
+        main_process.start()
+    else:
+        main_process.terminate()
+        toggle = False
+        main_process = None
+        print('Script telah dihentikan')
+        toggle_button.config(text='Mulai')
+
+def test_input_button():
+    global pop_up
+    global image_sequence
+    global frames
+    global count
+
+    frames = 50
+    count = 0
+
+    pop_up = tk.Toplevel(window)
+    pop_up.title('')
+
+    
+    pic_label = ttk.Label(pop_up, text='Setelah klik ok, fokuskan window ke lost saga dan tunggu beberapa detik.\nJika chat box muncul seperti ini, berarti input bisa digunakan')
+    pic_label.grid(row=1, column=2)
+
+    image = ImageLabel(pop_up)
+    image.grid(row=2, column=2)
+    image.load('./img/input.gif')
+
+    oki = ttk.Button(pop_up, text='ok', command=lambda:test_input())
+    oki.grid(row=3, column=2)
+
+def test_input():
+    global pop_up
+    global input_process
+
+    pop_up.destroy()
+
+    input_process = multiprocessing.Process(target=the_actual_input)
+
+    input_process.start()
+
+def the_actual_input():
+    time.sleep(5)
+    pydirectinput.press('enter')
+    pydirectinput.typewrite('abc123', interval=0.05)
+
+def test_screenshot():
+    tk.messagebox.showinfo(message='Setelah klik ok, fokuskan window ke Lost Saga lalu tunggu beberapa detik. Setelah lebih dari 5/6 detik, screenshot akan muncul di folder img')
+    time.sleep(5)
+
+    img = get_win_ss()
+
+    if img:
+        img.save('./img/ss.png')
+    else:
+        tk.messagebox.showwarning(title='Warning', message='Tidak dapat screenshot Lost Saga karena proses tidak ditemukan')
+
+def tesseract_set():
+    filename = filedialog.askopenfilename(initialdir = "/",title = "Select A File",filetype = (("exe","*.exe"),("All Files","*.*")))
+    if filename:
+        tesseract_loc.set(filename)
+
+if __name__ == '__main__': # stupid multiprocessing
+    window = tk.Tk()
+    s = ttk.Style(window)
+
+    try:
+        s.theme_use('vista')
+    except Exception:
+        pass
+
+    window.iconbitmap('img/sire icon.ico')
+    window.title('AFK solver')
+    tk.Grid.columnconfigure(window, 0, weight=1)
+    tk.Grid.rowconfigure(window, 0, weight=1)
+
+    mainframe = tk.Frame(window)
+    mainframe.grid(column=0, row=0, sticky=tk.N+tk.W+tk.E+tk.S)
+
+    for row_index in range(10):
+        tk.Grid.rowconfigure(mainframe, row_index, weight=1)
+        for col_index in range(4):
+            tk.Grid.columnconfigure(mainframe, col_index, weight=1)
+
+    tesseract_label = ttk.Label(mainframe, compound=tk.RIGHT, text='Lokasi Tesseract')
+    tesseract_label.grid(row=1, column=1, sticky=tk.E)
+
+    tesseract_loc = tk.StringVar(value=config.get('tesseract', ''))
+
+    tesseract_entry = ttk.Entry(mainframe, width=45, textvariable=tesseract_loc)
+    tesseract_entry.grid(row=1, column=2, sticky=tk.W)
+
+    browse = ttk.Button(mainframe, text='Browse',width=15, command=lambda:tesseract_set())
+    browse.grid(row=1, column=3, sticky=tk.W)
+
+    interval_input = tk.StringVar(value=config.get('interval', '5'))
+
+    interval_label = ttk.Label(mainframe, text='Interval screenshot (dalam detik)')
+    interval_label.grid(row=2, column=1, sticky=tk.E)
+
+    interval_entry = ttk.Entry(mainframe, width=5, textvariable=interval_input)
+    interval_entry.grid(row=2, column=2, sticky=tk.W)
+
+    interval_note = ttk.Label(mainframe, text='Masukkan 0 untuk screenshot\nsesegeranya, tidak direkomendasikan', foreground='red', font=('', 7))
+    interval_note.grid(row=2, column=3, sticky=tk.W+tk.S)
+
+    ttk.Label(mainframe, text='').grid(row=3, column=1)
+
+    fix_input = tk.StringVar(value=config.get('fix', 'n'))
+
+    fix_label = ttk.Label(mainframe, text='Fix (untuk yang tidak work\ndi script namun work di jitbit)')
+    fix_label.configure(anchor='center')
+    fix_label.grid(row=4, column=1)
+
+    ttk.Radiobutton(mainframe, text='Keyboard', value='k', variable=fix_input).grid(row=5, column=1)
+    ttk.Radiobutton(mainframe, text='None', value='n', variable=fix_input).grid(row=6, column=1)
+
+    input_input = tk.StringVar(value=config.get('input', 'directinput'))
+
+    input_label = ttk.Label(mainframe, text='Tipe input')
+    input_label.grid(row=4, column=2)
+
+    ttk.Radiobutton(mainframe, text='Default (Pynput)', value='default', variable=input_input).grid(row=5, column=2)
+    ttk.Radiobutton(mainframe, text='DirectInput', value='directinput', variable=input_input).grid(row=6, column=2)
+
+    window_input = tk.StringVar(value=config.get('window', 'window'))
+
+    window_label = ttk.Label(mainframe, text='Mode window')
+    window_label.grid(row=4, column=3)
+
+    ttk.Radiobutton(mainframe, text='Fullscreen', value='full', variable=window_input).grid(row=5, column=3)
+    ttk.Radiobutton(mainframe, text='Windowed', value='window', variable=window_input).grid(row=6, column=3)
+
+    ttk.Label(mainframe, text='').grid(row=7, column=2)
+
+    toggle_button = ttk.Button(mainframe, text='Mulai', width=15, command=lambda:toggle_script())
+    toggle_button.grid(row=8, column=2)
+
+    toggle_label = ttk.Label(mainframe, text='F6 shortcut untuk mulai/stop', foreground='green')
+    toggle_label.grid(row=8, column=3, sticky=tk.W)
+
+    input_test_button = ttk.Button(mainframe, text='Test input', width=15, command=lambda:test_input_button())
+    input_test_button.grid(row=8, column=1)
+
+    ss_test_button = ttk.Button(mainframe, text='Test screenshot', width=15, command=lambda:test_screenshot())
+    ss_test_button.grid(row=9, column=1)
+
+    version_label = ttk.Label(mainframe, text='Versi 0.2', foreground='blue', font=('', 7))
+    version_label.grid(row=12, column=1, sticky=tk.W+tk.S)
+    
+    resolution_input = tk.StringVar(value=config.get('resolution', '1280x720'))
+
+    reso_label = ttk.Label(mainframe, text='Resolusi window')
+    reso_label.grid(row=4, column=4)
+
+    resolutions = ['800x600', '1024x768', '1280x720', '1280x1024', '1600x900', '1680x1050', '1920x1080']
+    row_start = 4
+    
+    for x in resolutions:
+        row_start += 1
+        ttk.Radiobutton(mainframe, text=x, value=x, variable=resolution_input).grid(row=row_start, column=4)
+
+    for child in mainframe.winfo_children(): 
+        child.grid_configure(padx=4, pady=10)
+
+    listener = pynput.keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+
+    window.mainloop()
